a75392fece50f51b48e8a798fb85aeb57ba93330
suziesu <xs350@nyu.edu>
2015-04-15 14:25:51 -0400

    connect to detail

--- Hackathon/Hackathon/jobDetailViewController.swift
+++ Hackathon/Hackathon/jobDetailViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class jobDetailViewController: UIViewController {
 
+    @IBOutlet weak var companyLogo: UIImageView!
+    @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var jobTitleLabel: UILabel!
+    @IBOutlet weak var jobRoleTagLabel: UILabel!
+    @IBOutlet weak var jobTypeLabel: UILabel!
+    @IBOutlet weak var locationTagLabel: UILabel!
+    @IBOutlet weak var salaryLabel: UILabel!
+    @IBOutlet weak var skillTageText: UILabel!
+    @IBOutlet weak var jobAndCompanyText: UITextView!
+    
+    
+    //if have time could impletement this to mark as interested
+    @IBOutlet weak var markAsInterestedButton: UIButton!
+    @IBAction func markAsInterested(sender: UIButton) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
