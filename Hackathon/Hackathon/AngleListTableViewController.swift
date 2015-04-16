07d896ee96a1da02535f19ad88518d7c6304f6f9
chenkanck <chemkanck1990@gmail.com>
2015-04-16 15:22:50 -0400

    add refresh

--- Hackathon/Hackathon/AngleListTableViewController.swift
+++ Hackathon/Hackathon/AngleListTableViewController.swift
@@ -28,48 +28,102 @@ class AngleListTableViewController: UITableViewController , UISearchBarDelegate,
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        for i in 1...1{
-            var requestItem = request(.GET, "https://api.angel.co/1/jobs?access_token=ea35df8ec5d8542d06b196d4a50bba2ac34616d448bfd3c0&page=\(i)")
-            
-            requestItem.responseJSON{
-                (request,response,data,error) in
-                var json = JSON(data!)
-
-                for (key: String, subJson: JSON) in json["jobs"] {
-                    var newData: CellData?
-                    //Job info
-                    let jobTitle = subJson["title"].string ?? ""
-                    let jobType = subJson["job_type"].string ?? ""
-                    let jobDesc = subJson["description"].string ?? ""
-                    let createAt = subJson["created_at"].string ?? ""
-                    let updateAt = subJson["updated_at"].string ?? ""
-                    let salaryMin = subJson["salary_min"].string ?? ""
-                    let salaryMax = subJson["salary_max"].string ?? ""
-                    let angellistURL = subJson["angellist_url"].string ?? ""
-                    //company info
-                    let companyName = subJson["startup"]["name"].string ?? ""
-                    let companyFDesc = subJson["startup"]["product_desc"].string ?? ""
-                    let companyHDesc = subJson["startup"]["high_concept"].string ?? ""
-                    let img = subJson["startup"]["logo_url"].string ?? "no image"
-                    let companyURL = subJson["startup"]["company_url"].string ?? ""
-                    //tags info
-                    var tags = [tag]()
-                    for (keyTag: String, eachtag: JSON) in subJson["tags"]{
-                        let tagType = eachtag["tag_type"].string ?? ""
-                        let tagName = eachtag["display_name"].string ?? ""
-                        tags.append(tag(tagType: tagType, tagName: tagName))
-                    }
-                    
-                    newData = CellData(jobTitle: jobTitle, jobType: jobType, createdAt: createAt, updatedAt: updateAt, salaryMin: salaryMin, salaryMax: salaryMax, jobDesc: jobDesc, angellistURL: angellistURL, companyName: companyName, companyFDesc: companyFDesc, companyHDesc: companyHDesc, companyLogoURL: img, companyURL: companyURL, tags: tags)
-                    if newData != nil {
-                        self.myData.append(newData!)
-                    }
-                }
-                self.tableView.reloadData()
-            }//end closure
-        }
-        
+        updateData()
+       
         configureSegment()
+        
+        var tableRefreshController = UIRefreshControl()
+        tableRefreshController.addTarget(self, action: "handleRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        
+        self.refreshControl = tableRefreshController
+    }
+    
+    func handleRefresh() {
+        self.myData = []
+        
+        var requestItem = request(.GET, "https://api.angel.co/1/jobs?access_token=ea35df8ec5d8542d06b196d4a50bba2ac34616d448bfd3c0&page=\(1)")
+        
+        requestItem.responseJSON{(request,response,data,error) in
+            var json = JSON(data!)
+            
+            for (key: String, subJson: JSON) in json["jobs"] {
+                var newData: CellData?
+                //Job info
+                let jobTitle = subJson["title"].string ?? ""
+                let jobType = subJson["job_type"].string ?? ""
+                let jobDesc = subJson["description"].string ?? ""
+                let createAt = subJson["created_at"].string ?? ""
+                let updateAt = subJson["updated_at"].string ?? ""
+                let salaryMin = subJson["salary_min"].string ?? ""
+                let salaryMax = subJson["salary_max"].string ?? ""
+                let angellistURL = subJson["angellist_url"].string ?? ""
+                //company info
+                let companyName = subJson["startup"]["name"].string ?? ""
+                let companyFDesc = subJson["startup"]["product_desc"].string ?? ""
+                let companyHDesc = subJson["startup"]["high_concept"].string ?? ""
+                let img = subJson["startup"]["logo_url"].string ?? "no image"
+                let companyURL = subJson["startup"]["company_url"].string ?? ""
+                //tags info
+                var tags = [tag]()
+                for (keyTag: String, eachtag: JSON) in subJson["tags"]{
+                    let tagType = eachtag["tag_type"].string ?? ""
+                    let tagName = eachtag["display_name"].string ?? ""
+                    tags.append(tag(tagType: tagType, tagName: tagName))
+                }
+                
+                newData = CellData(jobTitle: jobTitle, jobType: jobType, createdAt: createAt, updatedAt: updateAt, salaryMin: salaryMin, salaryMax: salaryMax, jobDesc: jobDesc, angellistURL: angellistURL, companyName: companyName, companyFDesc: companyFDesc, companyHDesc: companyHDesc, companyLogoURL: img, companyURL: companyURL, tags: tags)
+                if newData != nil {
+                    self.myData.append(newData!)
+                }
+            }//for
+            self.refreshControl?.endRefreshing()
+            self.tableView.reloadData()
+        }//end closure
+        
+
+    }
+    
+    private func updateData() {
+        self.myData = []
+        var requestItem = request(.GET, "https://api.angel.co/1/jobs?access_token=ea35df8ec5d8542d06b196d4a50bba2ac34616d448bfd3c0&page=\(1)")
+        
+        requestItem.responseJSON{(request,response,data,error) in
+            var json = JSON(data!)
+            
+            for (key: String, subJson: JSON) in json["jobs"] {
+                var newData: CellData?
+                //Job info
+                let jobTitle = subJson["title"].string ?? ""
+                let jobType = subJson["job_type"].string ?? ""
+                let jobDesc = subJson["description"].string ?? ""
+                let createAt = subJson["created_at"].string ?? ""
+                let updateAt = subJson["updated_at"].string ?? ""
+                let salaryMin = subJson["salary_min"].string ?? ""
+                let salaryMax = subJson["salary_max"].string ?? ""
+                let angellistURL = subJson["angellist_url"].string ?? ""
+                //company info
+                let companyName = subJson["startup"]["name"].string ?? ""
+                let companyFDesc = subJson["startup"]["product_desc"].string ?? ""
+                let companyHDesc = subJson["startup"]["high_concept"].string ?? ""
+                let img = subJson["startup"]["logo_url"].string ?? "no image"
+                let companyURL = subJson["startup"]["company_url"].string ?? ""
+                //tags info
+                var tags = [tag]()
+                for (keyTag: String, eachtag: JSON) in subJson["tags"]{
+                    let tagType = eachtag["tag_type"].string ?? ""
+                    let tagName = eachtag["display_name"].string ?? ""
+                    tags.append(tag(tagType: tagType, tagName: tagName))
+                }
+                
+                newData = CellData(jobTitle: jobTitle, jobType: jobType, createdAt: createAt, updatedAt: updateAt, salaryMin: salaryMin, salaryMax: salaryMax, jobDesc: jobDesc, angellistURL: angellistURL, companyName: companyName, companyFDesc: companyFDesc, companyHDesc: companyHDesc, companyLogoURL: img, companyURL: companyURL, tags: tags)
+                if newData != nil {
+                    self.myData.append(newData!)
+                }
+            }
+             self.tableView.reloadData()
+        }//end closure
+    
+
     }
     
     private func configureSegment () {
